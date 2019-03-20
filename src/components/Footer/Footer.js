3b4358af65291720666163e2e2f659784ba8fd4b
Elliott, Geoffrey R <geoff.elliott@pnnl.gov>
2019-03-19 23:37:54 -0700

    Replacing default license from gatsbyjs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,6 +10,8 @@ export default () => {
                     <li>Social Links</li>
                     <li>Etc</li>
                 </ul>
+
+                <p><small><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span property="dct:title">The Crow's Nest</span> by <span property="cc:attributionName">Geoff Elliott</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</small></p>
             </div>
         </footer>
     )
