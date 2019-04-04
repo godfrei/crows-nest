013f2f188d4257a4c5b8bf15afe60f7052759514
Elliott, Geoffrey R <geoff.elliott@pnnl.gov>
2019-04-03 22:44:42 -0700

    Added Concussion Rifle Commando WAX

--- src/pages/waxes.js
+++ src/pages/waxes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, Link, withPrefix } from "gatsby"
+import { graphql, withPrefix } from "gatsby"
 import Helmet from "react-helmet"
 import config from "../../data/SiteConfig"
 import Layout from "../components/layout"
