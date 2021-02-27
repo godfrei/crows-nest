ceb0de63ac193cd85aeaccd8ca1e3e5dca55b19a
Elliott, Geoffrey R <geoff.elliott@pnnl.gov>
2021-02-27 12:31:09 -0800

    Fix image path

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../layout/index'
 import config from '../../data/SiteConfig'
+import trooper from '../images/trooper_sprite.png'
 
 const NotFoundPage = () => (
   <Layout>
     <main>
       <Helmet title={`You're not allowed here! | ${config.siteTitle}`} />
       <h1>You're not allowed here!</h1>
-      <img src="../images/trooper_sprite.png" />
+      <img src={trooper} alt="Stormtropper pointing his blaster at you." />
       <p>...because there's nothing here to see.</p>
     </main>
   </Layout>
