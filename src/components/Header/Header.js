1417dd160693b10b0395c9daab54619eba9609b6
Elliott, Geoffrey R <geoff.elliott@pnnl.gov>
2021-02-27 20:54:22 -0800

    Header (for now)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,14 +8,12 @@ import styles from './Header.module.scss'
 
 const Header = () => (
   <header className={styles.header}>
-    <div>
-      <h1>
-        <Link to="/" className={styles.title}>
-            {/* <img src={crow} alt="The Moldy Crow" id="crow" /> */}
-            {/* <GradientTitle title={config.siteTitle} /> */}
-            {config.siteTitle}
-        </Link>
-      </h1>
+    <div>      
+      <Link to="/" className={styles.title}>
+        <img src={crow} alt="The Moldy Crow" id="crow" />
+        <GradientTitle title={config.siteTitle} />
+        {/* <h1>{config.siteTitle}</h1> */}
+      </Link>
       <Navigation />
       {/* <Search /> */}
     </div>
