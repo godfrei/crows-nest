3f55f5c492147cfbb3d85b1c1c0c4426b46e268d
Geoff Elliott <godfrei@users.noreply.github.com>
2021-03-07 22:14:10 -0800

    Better review card (in progress)

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,21 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
-import { card } from "./ReviewCard.module.scss"
+import { link, card } from "./ReviewCard.module.scss"
+import defaultImage from '../../images/reviews.png'
 
 const ReviewCard = ({ review }) => {
+    console.log(review);
     let slug = review.node.fields.slug;
     slug = slug.slice(0, slug.indexOf('-review'));
     let title = review.node.frontmatter.title;
     title = title.slice(0, title.indexOf(' Review'));
+    const cover = (review.node.frontmatter.cover) ? review.node.frontmatter.cover.publicURL : defaultImage;
 
-    // const cover = (node.frontmatter.cover) ? node.frontmatter.cover.publicURL : "";
     return (
-        <Link to={slug} className={card}>
-            {/* <div className={cardStyles.cover} style={{ backgroundImage: `url(${cover})`}} /> */}
-            <strong>{title}</strong>
-            <small>Reviewed by {review.node.frontmatter.reviewers.join(', ')}</small>
-            <small>Rating: {review.node.frontmatter.rating}</small>
-            <p>{review.node.excerpt}</p>
+        <Link to={slug} className={link}>
+            <article className={card} style={{ backgroundImage: `url(${cover})` }}>
+                {/* <img src={cover} /> */}
+                <h1 title={title}>{title}</h1>
+                <p>{review.node.excerpt}</p>
+                <small>Reviewed by {review.node.frontmatter.reviewers.join(', ')}</small>
+                <small>Rating: {review.node.frontmatter.rating}</small>
+            </article>
         </Link>
     )
 }
