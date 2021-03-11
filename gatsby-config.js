48b2de520e0a1e51dec2ef289531a789e6982bef
Geoff Elliott <godfrei@users.noreply.github.com>
2021-03-10 21:42:56 -0800

    Mapping from the Review to the Mission
    But not the reverse

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,15 @@ module.exports = {
       copyright: config.copyright
     }
   },
+  // THIS DOESN'T WORK
+  // mapping: {
+  //   'MarkdownRemark.frontmatter.mission': `MarkdownRemark.frontmatter.mission_id`,
+  //   'MarkdownRemark.frontmatter.mission_id': `MarkdownRemark.frontmatter.mission`,
+  // },
+  mapping: {
+    'MarkdownRemark.frontmatter.mission': `MarkdownRemark.frontmatter.mission_id`,
+    // 'MarkdownRemark.frontmatter.mission_id': `MarkdownRemark.frontmatter.mission`,
+  },
   plugins: [
     
     "gatsby-plugin-react-helmet",
