978efd05b5991903ead8994cacaa6edc8099fe39
Geoff Elliott <godfrei@users.noreply.github.com>
2021-03-22 22:13:55 -0700

    Beginning Database layout

--- src/pages/database.js
+++ src/pages/database.js
@@ -1,38 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
 import Helmet from "react-helmet"
 import config from "../../data/SiteConfig"
 import SEO from '../components/SEO'
 import Layout from "../layout"
-import GradientTitle from "../components/GradientTitle"
-import database from "../images/database.png"
+import DatabaseLayout from '../layout/database';
+// import GradientTitle from "../components/GradientTitle"
+// import database from "../images/database.png"
 
 export default () => (
   <Layout>
-    <main>
-      <Helmet>
-        <title>Database | {config.siteTitle}</title>
-      </Helmet>
-      <SEO />
-      <img src={database} alt="" className="section_icon" />
-      <GradientTitle title="Database" />
-      
-      <h2>Dark Forces Info</h2>
+    <Helmet>
+      <title>Database | {config.siteTitle}</title>
+    </Helmet>
+    <SEO />
+    <DatabaseLayout>
+      <article>
+        <h1>General Info</h1>
 
-      <ul>
-        <li>General Info</li>
-        <li><Link to="demo">Demo</Link></li>
-        <li><Link to="enemies">Enemies</Link></li>
-        <li><Link to="weapons">Weapons</Link></li>
-        <li><Link to="cheats">Cheat Codes</Link></li>
-      </ul>
-
-      <h2>Add-Ons</h2>
-      
-      <ul>
-        <li><Link to="specs">Dark Forces Unofficial Specifications</Link></li>
-        <li>Launching Add-On Missions</li>
-      </ul>
-    </main>
+        <p>Stuff about <em>Dark Forces</em>, release, reception and where you can get it if you want to play.</p>
+      </article>
+    </DatabaseLayout>
   </Layout>
 )
--- src/pages/database/demo.js
+++ src/pages/database/demo.js
@@ -3,28 +3,30 @@ import Helmet from "react-helmet"
 import config from "../../../data/SiteConfig"
 import SEO from '../../components/SEO'
 import Layout from "../../layout"
+import DatabaseLayout from '../../layout/database';
 
 export default () => (
   <Layout>
-    <main>
-      <Helmet>
-        <title>Demo | {config.siteTitle}</title>
-      </Helmet>
-      <SEO />
-      
-      <h1>Demo</h1>
+    <Helmet>
+      <title>Demo | {config.siteTitle}</title>
+    </Helmet>
+    <SEO />
+    <DatabaseLayout>
+      <article>
+        <h1>Demo</h1>
 
-      <p>The demo for Dark Forces consists of the first mission of the game, in which you steal the plans for the Death Star, along with the opening and trailing cutscenes.</p>
+        <p>The demo for Dark Forces consists of the first mission of the game, in which you steal the plans for the Death Star, along with the opening and trailing cutscenes.</p>
 
-      <p> Running the demo is easy enough on Mac or Windows using <a href="https://dosbox.com">DOSBOX</a>.</p>
+        <p> Running the demo is easy enough on Mac or Windows using <a href="https://dosbox.com">DOSBOX</a>.</p>
 
-      <p>The files here are linked from <a href="https://archive.org">The Internet Archive</a>.</p>
+        <p>The files here are linked from <a href="https://archive.org">The Internet Archive</a>.</p>
 
-      <ul>
+        <ul>
           <li><a href="https://archive.org/download/StarWarsDarkForces/dforces1.zip">dforces1.zip</a> (3.2M) - This is the actual demo, and the only file you need to get in order to play.</li>
           <li><a href="https://archive.org/download/StarWarsDarkForces/dforces2.zip">dforces2.zip</a> (1.1M) - The opening cutscene, including the classic Star Wars text crawl. This is optional.</li>
           <li><a href="https://archive.org/download/StarWarsDarkForces/dforces3.zip">dforces3.zip</a> (5.6M) - The cutscene that follows the first mission and sets up the story for the rest of the game. This is also optional.</li>
-      </ul>
-    </main>
+        </ul>
+      </article>
+    </DatabaseLayout>
   </Layout>
 )
