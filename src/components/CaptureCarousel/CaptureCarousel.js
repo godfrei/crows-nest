102673b7de1b7046051b52d24f1682aa3aef4a21
Geoff Elliott <godfrei@users.noreply.github.com>
2021-04-18 12:31:42 -0700

    Add next/prev buttons, highlighting for active image

--- src/components/CaptureCarousel/CaptureCarousel.js
+++ src/components/CaptureCarousel/CaptureCarousel.js
@@ -10,14 +10,33 @@ const CaptureCarousel = ({ captures }) => {
     if (typeof newOffset === undefined || newOffset === null)
       newOffset = offset + 1;
     if (newOffset > captures.length - 1) {
-      setOffset(0);
+      newOffset = 0;
+    }
+    else if (newOffset < 0) {
+      newOffset = captures.length - 1;
     }
     setOffset(newOffset);
   };
 
+  const nextCapture = () => {
+    const newOffset = offset + 1;
+    updateOffset(newOffset);
+  }
+
+  const prevCapture = () => {
+    const newOffset = offset - 1;
+    updateOffset(newOffset);
+  }
+
   return (
     <>
       <div className={styles.captureScroll}>
+        <button onClick={prevCapture} className={styles.prev}>
+          <span className="sr-only">Previous</span>
+        </button>
+        <button onClick={nextCapture} className={styles.next}>
+          <span className="sr-only">Next</span>
+        </button>
         <ul
           className={`${styles.captureList} ${styles[stackClass]} ${
             styles[`offset${offset}`]
@@ -25,16 +44,20 @@ const CaptureCarousel = ({ captures }) => {
         >
           {captures.map((capture) => (
             <li>
-              <img src={capture.file.publicURL} alt="" />
-              <p>{capture.caption}</p>
+              <figure>
+                <img src={capture.file.publicURL} alt="" />
+                <figcaption>{capture.caption}</figcaption>
+              </figure>
             </li>
           ))}
         </ul>
       </div>
-      <ul className={styles.buttons}>
+      {captures.length > 1 && (
+        <ul className={styles.buttons}>
         {captures.map((capture, index) => {
+          const activeClass = index === offset ? styles.active : null;
           return (
-            <li>
+            <li className={activeClass}>
               <button onClick={() => updateOffset(index)}>
                 <img
                   src={capture.file.publicURL}
@@ -45,6 +68,7 @@ const CaptureCarousel = ({ captures }) => {
           );
         })}
       </ul>
+      )}
     </>
   );
 };
