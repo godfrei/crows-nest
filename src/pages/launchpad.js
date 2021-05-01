f5d8c31a0fd69cc4a8e6d10acd0cd294a59994b8
Geoff Elliott <godfrei@users.noreply.github.com>
2021-04-30 20:01:27 -0700

    Clean up Launch Pad

--- src/pages/launchpad.js
+++ src/pages/launchpad.js
@@ -2,74 +2,82 @@ import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../layout";
 import config from "../../data/SiteConfig";
+import { pageHeader, description, links } from './launchpad.module.scss';
 
 const LaunchpadPage = () => (
   <Layout>
     <Helmet title={`Launch Pad | ${config.siteTitle}`} />
 
-    <h1>Launch Pad</h1>
+    <div className={pageHeader}>
+      <div className={description}>
+        <h1>Launch Pad</h1>
 
-    <p>
-      If you're looking for links to other Dark Forces content around the web,
-      this is the spot. Sadly, most of the other sites have, like the Nest, shut
-      down in the intervening years. There are only a couple active places you
-      can find active Dark Forces content.
-    </p>
+        <p>
+          If you're looking for links to other Dark Forces content around the web,
+          this is the spot. Sadly, most of the other sites have, like the Nest, shut
+          down in the intervening years. There are only a couple active places you
+          can find active Dark Forces content.
+        </p>
 
-    <p>
-      But in the spirit of documenting history there are a bunch of expired
-      links here, some pointing to the Internet Archive if you want to take a
-      trip down memory lane.
-    </p>
+        <p>
+          But in the spirit of documenting history there are a bunch of expired
+          links here, some pointing to the Internet Archive if you want to take a
+          trip down memory lane.
+        </p>
+      </div>
+    </div>
+    
+    <div className={links}>
 
-    <h2>Active Sites</h2>
+      <h2>Active Sites</h2>
 
-    <ul>
-      <li>
-        <a href="https://df-21.net/">DF-21</a> - The most active community for
-        Dark Forces fans. This crew is and has been carrying the torch for years
-        and have a{" "}
-        <a href="https://discord.gg/6T9NvMh2MC">
-          <span className="sr-only">DF-21</span> Discord
-        </a>
-        ,{" "}
-        <a href="https://www.twitch.tv/df21net/">
-          <span className="sr-only">DF-21</span> Twitch
-        </a>{" "}
-        and{" "}
-        <a href="https://www.youtube.com/channel/UCG6YzOWwemLBZrajnEmOtGQ/featured">
-          <span className="sr-only">DF-21</span> YouTube
-        </a>{" "}
-        channels.
-      </li>
+      <ul>
+        <li>
+          <a href="https://df-21.net/">DF-21</a> - The most active community for
+          Dark Forces fans. This crew is and has been carrying the torch for years
+          and have a{" "}
+          <a href="https://discord.gg/6T9NvMh2MC">
+            <span className="sr-only">DF-21</span> Discord
+          </a>
+          ,{" "}
+          <a href="https://www.twitch.tv/df21net/">
+            <span className="sr-only">DF-21</span> Twitch
+          </a>{" "}
+          and{" "}
+          <a href="https://www.youtube.com/channel/UCG6YzOWwemLBZrajnEmOtGQ/featured">
+            <span className="sr-only">DF-21</span> YouTube
+          </a>{" "}
+          channels.
+        </li>
 
-      <li>
-        <a href="https://theforceengine.github.io/">The Force Engine</a> - A
-        project to reverse engineer the Jedi Engine used in Dark Forces (and
-        Outlaws) to enable easier play on more modern systems along with a whole
-        bunch of other goodies.
-      </li>
-    </ul>
+        <li>
+          <a href="https://theforceengine.github.io/">The Force Engine</a> - A
+          project to reverse engineer the Jedi Engine used in Dark Forces (and
+          Outlaws) to enable easier play on more modern systems along with a whole
+          bunch of other goodies.
+        </li>
+      </ul>
 
-    <h2>Vintage Sites</h2>
+      <h2>Vintage Sites</h2>
 
-    <ul>
-      <li>
-        <a href="http://web.archive.org/web/19990209054406/http://e-reality.com/DarkForces/">
-          The Dark Forces Homepage
-        </a>
-      </li>
-      <li>
-        <a href="http://web.archive.org/web/20021016152253/http://www.geocities.com/darkforces_faq/index.html">
-          Dark Forces FAQList
-        </a>
-      </li>
-      <li>
-        <a href="http://web.archive.org/web/20021208070833/http://homepage.mac.com/anewmanagn/magic_al/">
-          MagicAl's Dark Forces Niche
-        </a>
-      </li>
-    </ul>
+      <ul>
+        <li>
+          <a href="http://web.archive.org/web/19990209054406/http://e-reality.com/DarkForces/">
+            The Dark Forces Homepage
+          </a>
+        </li>
+        <li>
+          <a href="http://web.archive.org/web/20021016152253/http://www.geocities.com/darkforces_faq/index.html">
+            Dark Forces FAQList
+          </a>
+        </li>
+        <li>
+          <a href="http://web.archive.org/web/20021208070833/http://homepage.mac.com/anewmanagn/magic_al/">
+            MagicAl's Dark Forces Niche
+          </a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 );
 
